b506e20f2609c8e93a3be9fda669bbe6a9bc08e6
Cecil <kobebrainght@yahoo.com>
2016-11-25 16:22:15 +0100

    update on contact us page

--- contact_me.php
+++ contact_me.php
@@ -19,7 +19,7 @@ $message = $_POST['message'];
 $to = 'solmaz.naderi990@gmail.com'; // PUT YOUR EMAIL ADDRESS HERE
 $email_subject = "Martin Filters Website Contact Form:  $name"; // EDIT THE EMAIL SUBJECT LINE HERE
 $email_body = "You have received a new message from your website's contact form.\n\n"."Here are the details:\n\nName: $name\n\nPhone: $phone\n\nEmail: $email_address\n\nMessage:\n$message";
-//$headers = "From: noreply@your-domain.com\n";
+$headers = "From: noreply@your-domain.com\n";
 $headers = "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
