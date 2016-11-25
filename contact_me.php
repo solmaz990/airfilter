c9ff9ed44a490ec37b5b10bfe06d9b888eb41dce
Cecil <kobebrainght@yahoo.com>
2016-11-25 15:35:32 +0100

    updated contact page.

--- contact_me.php
+++ contact_me.php
@@ -16,7 +16,7 @@ $email_address = $_POST['email'];
 $message = $_POST['message'];
 	
 // create email body and send it	
-$to = 'your-email@domain.com'; // PUT YOUR EMAIL ADDRESS HERE
+$to = 'solmaz.naderi990@gmail.com'; // PUT YOUR EMAIL ADDRESS HERE
 $email_subject = "Martin Filters Website Contact Form:  $name"; // EDIT THE EMAIL SUBJECT LINE HERE
 $email_body = "You have received a new message from your website's contact form.\n\n"."Here are the details:\n\nName: $name\n\nPhone: $phone\n\nEmail: $email_address\n\nMessage:\n$message";
 //$headers = "From: noreply@your-domain.com\n";
