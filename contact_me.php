1f42f29df1198cd8cb794c2cc3afc1491ba081c2
Solmaz Naderi <solmaz.naderi90@outlook.com>
2016-11-26 06:48:33 +0000

    Update contact_me.php
    I translate all content except $headers = "From: noreply@your-domain.com\n";

--- contact_me.php
+++ contact_me.php
@@ -16,9 +16,9 @@ $email_address = $_POST['email'];
 $message = $_POST['message'];
 	
 // create email body and send it	
-$to = 'solmaz.naderi990@gmail.com'; // PUT YOUR EMAIL ADDRESS HERE
+$to = 'matin.filter@gmail.com'; // PUT YOUR EMAIL ADDRESS HERE
 $email_subject = "Martin Filters Website Contact Form:  $name"; // EDIT THE EMAIL SUBJECT LINE HERE
-$email_body = "You have received a new message from your website's contact form.\n\n"."Here are the details:\n\nName: $name\n\nPhone: $phone\n\nEmail: $email_address\n\nMessage:\n$message";
+$email_body = "\ایمیل دریافت شده از طرف وب سایت متین فیلتر می باشد.\n\n"."جزئیات:\n\nنام: $name\n\nشماره تماس: $phone\n\nآدرس ایمیل: $email_address\n\nپیام:\n$message";
 $headers = "From: noreply@your-domain.com\n";
 $headers = "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
